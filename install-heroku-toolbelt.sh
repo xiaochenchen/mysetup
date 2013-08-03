14b514537b859d31b683d9ae2d6eb40255813748
Xiaochen Chen <xiaochenchen.git@gmail.com>
2013-08-03 19:55:13 +0000

    add heroku, pathogen, vim-rails setup scripts

---
+++ install-heroku-toolbelt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
---
+++ install-vim-rails.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-rails.git
+git clone git://github.com/tpope/vim-bundler.git
