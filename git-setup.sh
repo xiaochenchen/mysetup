ce5a32846d06daff814a168cf292faec86c14dd9
Xiaochen Chen <xiaochenchen.git@gmail.com>
2014-02-05 23:29:35 -0500

    add color status to git setup

--- git-setup.sh
+++ git-setup.sh
@@ -10,6 +10,7 @@ git config --global core.editor vim
 # color ui
 git config --global color.ui true
 git config --global color.diff.meta "blue black bold"
+git config --global color.status always
 
 # log alias
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
